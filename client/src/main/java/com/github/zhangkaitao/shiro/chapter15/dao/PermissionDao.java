56528df3725773ad57b4bd088bab9c07cfdacdda
aiolosgs <aiolosgs.163.com>
2018-06-26 10:03:29 +0800

    git test

--- client/src/main/java/com/github/zhangkaitao/shiro/chapter15/dao/PermissionDao.java
+++ client/src/main/java/com/github/zhangkaitao/shiro/chapter15/dao/PermissionDao.java
@@ -6,6 +6,7 @@ import com.github.zhangkaitao.shiro.chapter15.entity.Permission;
  * <p>User: Zhang Kaitao
  * <p>Date: 14-1-28
  * <p>Version: 1.0
+
  */
 public interface PermissionDao {
 
--- client/src/main/java/com/github/zhangkaitao/shiro/chapter15/service/PermissionServiceImpl.java
+++ client/src/main/java/com/github/zhangkaitao/shiro/chapter15/service/PermissionServiceImpl.java
@@ -23,4 +23,8 @@ public class PermissionServiceImpl implements PermissionService {
     public void deletePermission(Long permissionId) {
         permissionDao.deletePermission(permissionId);
     }
+
+    private void doNothing(){
+        system.out.pringtln("hello the world !");
+    }
 }
